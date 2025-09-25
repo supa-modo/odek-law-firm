f8bb72d210a37e49eeac10ec68414c0188d3cb9f
supa-modo <eddieochieng11@gmail.com>
2025-09-25 22:28:28 +0300

    Update Vite configuration to set server port to 3000 and adjust Header component styles for improved responsiveness and visual consistency. Change background padding and text color based on scroll state for better user experience.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -140,7 +140,7 @@ const Header = () => {
       className={`fixed top-0 left-0 right-0 z-50 transition-all duration-500 ${
         isScrolled || isMenuOpen
           ? "bg-white shadow-lg"
-          : "bg-transparent py-3 md:py-4 lg:py-6"
+          : "bg-transparent py-1 md:py-2 lg:py-6"
       }`}
     >
       <div className="w-full mx-auto px-4 sm:px-6 lg:px-14">
@@ -159,7 +159,7 @@ const Header = () => {
                     className={`text-xl lg:text-2xl font-extrabold font-garamond transition-colors duration-300 ${
                       isScrolled || isMenuOpen
                         ? "text-primary-700"
-                        : "text-white"
+                        : "text-secondary-400"
                     }`}
                   >
                     Obel & Company Associates
